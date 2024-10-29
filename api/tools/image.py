fff4bc69fef4ec53f7d97fca1753262d00e9d838
SpritePlusMilk <r1m_musin@mail.ru>
2024-10-29 00:11:45 -0700

    # вью, gitlab ci, функция для преобразования и создания изображения

---
+++ api/tools/image.py
@@ -0,0 +1,36 @@
+import os
+from typing import TYPE_CHECKING
+
+from PIL import Image
+
+from api.consts import IMAGE_STORE_PATH, RESOLUTION_MEDIUM
+from api.models import Image as Image_
+
+if TYPE_CHECKING:
+    from django.core.files.uploadedfile import TemporaryUploadedFile
+
+
+def get_size(resolution: str) -> tuple[int, int]:
+    x_position = resolution.find('x')
+    width = int(resolution[:x_position])
+    height = int(resolution[x_position + 1 :])
+
+    return width, height
+
+
+def create_image(file: 'TemporaryUploadedFile', resolution: str = RESOLUTION_MEDIUM) -> Image:
+    file_name, extension = os.path.splitext(file.name)
+    same_name_count = Image_.objects.filter(name__startswith=file_name, name__endswith=extension).count()
+    image_name = f'{file_name} ({same_name_count}){extension}' if same_name_count else file.name
+    image_path = IMAGE_STORE_PATH + image_name
+
+    with open(image_path, 'wb+') as uploaded_image:
+        for chunk in file.chunks():
+            uploaded_image.write(chunk)
+
+    with Image.open(image_path) as image:
+        image.convert('L')
+        image.thumbnail((get_size(resolution)), Image.Resampling.BOX)
+        image.save(image_path)
+
+    return Image_.objects.create(name=image_name, file=image_path, resolution=resolution, size=file.size)
--- api/views.py
+++ api/views.py
@@ -1,30 +1,39 @@
+from typing import TYPE_CHECKING
+
 from drf_spectacular.utils import extend_schema, inline_serializer
 from rest_framework import status
 from rest_framework.decorators import action
+from rest_framework.exceptions import APIException
 from rest_framework.fields import CharField, ImageField
-from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from api.consts import IMAGE_STORE_PATH
 from api.models import Image
 from api.serializers import ImageSerializer
+from api.tools.image import create_image
+
+if TYPE_CHECKING:
+    from django.core.files.uploadedfile import TemporaryUploadedFile
+    from django.db.models import QuerySet
+    from rest_framework.request import Request
+    from rest_framework.serializers import Serializer
 
 
 class ImageViewSet(ModelViewSet):
-    queryset = Image.objects.all()
-    serializer_class = ImageSerializer
+    queryset: 'QuerySet[Image]' = Image.objects.all()
+    serializer_class: 'Serializer' = ImageSerializer
 
     @extend_schema(request=inline_serializer(name='Upload', fields={'resolution': CharField(), 'file': ImageField()}))
     @action(detail=False, methods=['POST'])
-    def upload(self, request: Request) -> Response:
-        try:
-            file = request.FILES['file']
-            with open(f'{IMAGE_STORE_PATH}/' + file.name, 'wb+') as uploaded_file:
-                for chunk in file.chunks():
-                    uploaded_file.write(chunk)
-            return Response(file.name, status.HTTP_201_CREATED)
+    def upload(self, request: 'Request') -> Response:
+        # try:
+        file: TemporaryUploadedFile = request.FILES['file']
 
-        except Exception as exc:
-            print(exc)
-            return Response(exc, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        if file and 'image' in file.content_type:
+            image: Image = create_image(file)
+            return Response(image.name, status.HTTP_201_CREATED)
+        raise APIException('Отправленный файл не является изображением')
+        #
+        # except Exception as exc:
+        #     print(exc)
+        #     return Response(exc, status.HTTP_500_INTERNAL_SERVER_ERROR)
