d1ada934ff20946a17c4ac11909b97167515a5bf
r.musin <r1m_musin@mail.ru>
2024-10-29 21:41:35 +0500

    # Добавил JWT и отправку сообщений в брокер при загрузке, обновлении и удалении изображения через API

--- api/views.py
+++ api/views.py
@@ -1,10 +1,12 @@
 from typing import TYPE_CHECKING, Type
 
-from rest_framework.viewsets import ModelViewSet
+from django.utils.timezone import now
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.viewsets import ModelViewSet
 
 from api.models import Image
 from api.serializers import ImageRead, ImageWrite
+from project.rabbitmq import produce_message
 
 if TYPE_CHECKING:
     from django.db.models import QuerySet
@@ -24,3 +26,13 @@ class ImageViewSet(ModelViewSet):
         serializer.is_valid(raise_exception=True)
         image = serializer.save(size=self.request.FILES['file'].size)
         # process_image(image)
+        produce_message(f'{image.upload_dt} создано изображение "{image.name}" ({image.file.path})')
+
+    def perform_update(self, serializer: 'Serializer') -> None:
+        super().perform_update(serializer)
+        image = self.get_object()
+        produce_message(f'Изображение "{image.name}" ({image.file.path}) было обновлено {now()}')
+
+    def perform_destroy(self, instance: Image) -> None:
+        produce_message(f'Изображение "{instance.name}" ({instance.file.path}) было удалено {now()}')
+        super().perform_destroy(instance)
--- project/settings.py
+++ project/settings.py
@@ -92,7 +92,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Настройки DRF
 REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
-    'DEFAULT_AUTHENTICATION_CLASSES': 'rest_framework_simplejwt.authentication.JWTAuthentication'
+    'DEFAULT_AUTHENTICATION_CLASSES': 'rest_framework_simplejwt.authentication.JWTAuthentication',
 }
 
 # Настройки drf-spectacular
